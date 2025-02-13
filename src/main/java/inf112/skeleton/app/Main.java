46c407ec133f1f84e87d62eb4f0304083e3bb1c4
Eivind Naasen <einaa7293@uib.no>
2025-02-13 15:41:24 +0100

    more test

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -6,11 +6,9 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
-        cfg.setTitle("dinosaurspill - LOL");
+        cfg.setTitle("Mario 2");
         cfg.setWindowedMode(480, 320);
-        System.out.println("Endre var her");
 
         new Lwjgl3Application(new HelloWorld(), cfg);
-        // Eivind branch test
     }
 }
