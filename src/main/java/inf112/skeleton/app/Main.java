e8f23457f77f384e9733cf17d6093c588492403b
Eivind Naasen <einaa7293@uib.no>
2025-02-13 13:09:45 +0100

    Cleaned some unecessary whitespace and an import in Main.java

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -2,11 +2,6 @@ package inf112.skeleton.app;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.badlogic.gdx.math.Vector2;
-
-
-
-
 
 public class Main {
     public static void main(String[] args) {
