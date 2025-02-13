5873415b15f142d634bccfe452717fe752a7b7b4
Eivind Naasen <einaa7293@uib.no>
2025-02-13 15:58:35 +0100

    Even more testing

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -10,5 +10,6 @@ public class Main {
         cfg.setWindowedMode(480, 320);
 
         new Lwjgl3Application(new HelloWorld(), cfg);
+        System.out.println("Eivind er dum i hode sitt");
     }
 }
