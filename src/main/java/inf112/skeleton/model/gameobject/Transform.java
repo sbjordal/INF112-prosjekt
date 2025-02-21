84b96411da56805bed18b1679b02c35b6b2eac78
Thale <thbje9813@uib.no>
2025-02-21 14:03:52 +0100

    lagt til transform klasse+ kommentarer

--- src/main/java/inf112/skeleton/model/gameobject/Transform.java
+++ src/main/java/inf112/skeleton/model/gameobject/Transform.java
@@ -1,4 +1,12 @@
 package inf112.skeleton.model.gameobject;
 
-public record Transform() {
+/**
+ * Simple record class that describes where an object is on the screen
+ * and its size
+ *
+ * @param pos Position object
+ * @param size Size object
+ */
+
+public record Transform(Position pos, Size size) {
 }
