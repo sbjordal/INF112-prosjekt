cb27f80924e1007c73fcd6e9bdf23d01dc2b16ab
Thale <thbje9813@uib.no>
2025-02-21 13:10:15 +0100

    lagt inn javadoc + to do kommentar

--- src/main/java/inf112/skeleton/model/gameobject/Position.java
+++ src/main/java/inf112/skeleton/model/gameobject/Position.java
@@ -2,11 +2,10 @@ package inf112.skeleton.model.gameobject;
 
 /**
  * Simple record class with coordinates on the play board
- * which represents the position of some object
  *
  * @param x x coordinate
  * @param y y coordinate
  */
 
-public record Position(int x, int y) {
+public record Position(int x, int y) { //TO DO: velge datatypen til kordinatene
 }
