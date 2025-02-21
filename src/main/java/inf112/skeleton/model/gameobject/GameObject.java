f9a11994463ecf3445a57982613dd9ae6573bde5
Thale <thbje9813@uib.no>
2025-02-21 13:08:40 +0100

    laget konstuktør + kommentert

--- src/main/java/inf112/skeleton/model/gameobject/GameObject.java
+++ src/main/java/inf112/skeleton/model/gameobject/GameObject.java
@@ -1,4 +1,25 @@
 package inf112.skeleton.model.gameobject;
+import com.badlogic.gdx.graphics.Texture;
 
+/**
+ * This class represents every object that appears on the play screen.
+ *
+ */
 public class GameObject {
+    protected Position position;
+    protected Texture sprite;
+
+    /**
+     * Constructor that creates a game object based on its position
+     * and texture
+     *
+     * @param position current position of object
+     * @param sprite Texture object
+     */
+
+    public GameObject(Position position, Texture sprite){
+        this.position = position;
+        this.sprite = sprite;
+
+    }
 }
--- src/main/java/inf112/skeleton/model/gameobject/Position.java
+++ src/main/java/inf112/skeleton/model/gameobject/Position.java
@@ -1,4 +1,12 @@
 package inf112.skeleton.model.gameobject;
 
+/**
+ * Simple record class with coordinates on the play board
+ * which represents the position of some object
+ *
+ * @param x x coordinate
+ * @param y y coordinate
+ */
+
 public record Position(int x, int y) {
 }
