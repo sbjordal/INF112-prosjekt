5745db4011787e92576b84bc87353273c933ec26
Stine.Bjordal <stbjo9442@uib.no>
2025-02-22 11:05:52 +0100

    Skrevet position og scorable, startet såvidt på worldmodel (må finne ut av logikk før vi kommer videre)

--- src/main/java/inf112/skeleton/model/WorldModel.java
+++ src/main/java/inf112/skeleton/model/WorldModel.java
@@ -1,13 +1,20 @@
 package inf112.skeleton.model;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.graphics.Texture;
 import inf112.skeleton.controller.ControllableWorldModel;
+import inf112.skeleton.model.gameobject.Position;
+import inf112.skeleton.model.gameobject.mobileobject.EnemySpawner;
 import inf112.skeleton.model.gameobject.mobileobject.Player;
+import inf112.skeleton.view.ViewableWorldModel;
 
-public class WorldModel implements ControllableWorldModel, ApplicationListener {
+import java.util.List;
+
+public class WorldModel implements ViewableWorldModel, ControllableWorldModel, ApplicationListener {
 
     private GameState gameState;
     private Player player;
+//    private int gameScore;
 
     public WorldModel() {
         this.gameState = GameState.GAME_ACTIVE; // TODO, må endres etter at game menu er laget.
@@ -17,7 +24,7 @@ public class WorldModel implements ControllableWorldModel, ApplicationListener {
     /**
      * Checks if MobileObject can be moved where it wants to move or not.
      *
-     * @return true if yes, false if no.
+     * @return True if the position is legal, false otherwise
      */
     private boolean isLegalMove() {
         return false; // TODO, implement me :)
@@ -69,6 +76,31 @@ public class WorldModel implements ControllableWorldModel, ApplicationListener {
         // TODO, implement me :)
     }
 
+    @Override
+    public Texture getEnemySprite() {
+        return null;
+    }
+
+    @Override
+    public Texture getPlayerSprite() {
+        return null;
+    }
+
+    @Override
+    public List<Position> getFixedObjectPosition() {
+        return List.of();
+    }
+
+    @Override
+    public List<Position> getEnemysPosition() {
+        return List.of();
+    }
+
+    @Override
+    public Position getPlayerPosition() {
+        return null;
+    }
+
     /**
      * Tells us the state of the game
      * @return the state of the game
@@ -76,6 +108,11 @@ public class WorldModel implements ControllableWorldModel, ApplicationListener {
     @Override
     public GameState getGameState() {
         return this.gameState;
+    }
+
+    @Override
+    public int getScore() {
+        return 0;
     }
 
 }
