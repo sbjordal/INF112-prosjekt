eb7c5434ed90e887b6eee1678c5b931f30fd27f9
Eivind Naasen <eivindnaasen@gmail.com>
2025-03-01 09:58:41 +0100

    Modified GameState javadoc.

--- src/main/java/inf112/skeleton/model/GameState.java
+++ src/main/java/inf112/skeleton/model/GameState.java
@@ -1,6 +1,12 @@
 package inf112.skeleton.model;
 
-// TODO: comment me :)
+/**
+ * Represents the different states of the game.
+ * {@link #GAME_ACTIVE}     The game is currently running.
+ * {@link #GAME_PAUSED}     The game is temporarily paused.
+ * {@link #GAME_OVER}       The game has ended.
+ * {@link #GAME_MENU}       The game is in the main menu.
+ */
 public enum GameState {
     GAME_ACTIVE,
     GAME_PAUSED,
