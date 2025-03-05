1c27f914f276f86f53b96c28d41372f11f7c0956
Julie Sandanger <jusan2762@uib.no>
2025-03-05 17:37:20 +0100

    endret forholdstall bakgrunn

--- src/main/java/inf112/skeleton/view/ParallaxBackground.java
+++ src/main/java/inf112/skeleton/view/ParallaxBackground.java
@@ -15,7 +15,7 @@ public class ParallaxBackground {
     public ParallaxBackground() {
         layers = new Texture[5];
         layerX = new float[5];
-        scrollSpeeds = new float[]{0.4f, 0.8f, 1.6f, 2.4f, 4.0f};
+        scrollSpeeds = new float[]{0.4f, 0.8f, 2.0f, 4.0f, 8.0f};
     }
     public void loadTextures(){
        for (int i = 0; i < 5; i++) {
