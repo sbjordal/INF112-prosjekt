fc353baf32c5de1862abfb36f16248a937257971
Stine.Bjordal <stbjo9442@uib.no>
2025-03-19 09:08:39 +0100

    Skrevet tester for player

--- src/test/java/inf112/skeleton/app/model/gameobject/mobileobject/actor/PlayerTest.java
+++ src/test/java/inf112/skeleton/app/model/gameobject/mobileobject/actor/PlayerTest.java
@@ -10,5 +10,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
+    private Player player;
+    private Transform transform;
+
+    @BeforeEach
+    void setUp() {
+        // Opprette en transform med en posisjon og størrelse
+        transform = new Transform(new Vector2(10, 20), new Vector2(30, 30));
+        // Initialisere player med 3 liv og hastighet på 5
+        player = new Player(3, 5, transform);
+    }
+
+    @Test
+    void testPlayerCreation() {
+        assertNotNull(player, "Player should not be null");
+        assertEquals(3, player.getLives(), "Player should have 3 lives initially");
+        assertEquals(5, player.getMovementSpeed(), "Player should have a movement speed of 5");
+    }
+
+    @Test
+    void testJumpSetsVerticalVelocity() {
+        int jumpForce = 10;
+        player.jump(jumpForce);
+
+        assertEquals(jumpForce, player.getVerticalVelocity(), "Vertical velocity should match the jump force");
+    }
+
+    @Test
+    void testHasPowerUp() {
+        assertFalse(player.getHasPowerUp(), "Player should not have power-up initially");
+
+        player.setHasPowerUp(true);
+        assertTrue(player.getHasPowerUp(), "Player should have power-up after setting it to true");
+
+        player.setHasPowerUp(false);
+        assertFalse(player.getHasPowerUp(), "Player should not have power-up after setting it to false");
+    }
+
+    @Test
+    void testPlayerLives() {
+        assertEquals(3, player.getLives(), "Player should have 3 lives initially");
+
+    }
+
 
 }
