bcd6e7d42302d9f9a1ca41099908c9a1e462fd9f
Eivind Naasen <eivindnaasen@gmail.com>
2025-03-21 21:03:58 +0100

    New javadoc for loadLevel(). Rearranged some of its logic.

--- src/main/java/inf112/skeleton/model/LevelManager.java
+++ src/main/java/inf112/skeleton/model/LevelManager.java
@@ -9,11 +9,9 @@ import inf112.skeleton.model.gameobject.GameObject;
 import inf112.skeleton.model.gameobject.Transform;
 import inf112.skeleton.model.gameobject.fixedobject.FixedObject;
 import inf112.skeleton.model.gameobject.fixedobject.item.ItemFactory;
-import inf112.skeleton.model.gameobject.mobileobject.actor.Player;
 import inf112.skeleton.model.gameobject.mobileobject.actor.enemy.EnemyFactory;
 import inf112.skeleton.model.gameobject.mobileobject.actor.enemy.EnemyType;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -44,63 +42,75 @@ public class LevelManager {
         LEVEL_3
     }
 
+    /**
+     * Load a level.
+     * The level is represented as a list of {@link GameObject} types.
+     * The game objects are extracted from a JSON file by parsing it.
+     *
+     * @param level     Level to load.
+     * @return          The extracted game objects as a list.
+     * @throws IllegalStateException If anything other than exactly one player or exactly one star was found.
+     */
     public static ArrayList<GameObject> loadLevel(Level level) {
         FileHandle levelFile = getLevelFile(level);
-        ArrayList<GameObject> objects = new ArrayList<>();
         ObjectMapper objectMapper = new ObjectMapper();
+        String levelContent;
+        JsonNode jsonRoot;
 
-        // TODO: kanskje flytt hele try-catch statement før heile greien.
         // TODO: fiks file path til å fungere med getLevelFile().
         // TODO: få player til å bli inkludert her istedenfor i WorldModel.
         // TODO: legg til star.
-        // TODO: legg til template prosjekt (Tiled) for level-designer til å bruke.
         try {
-            String levelContent = levelFile.readString();
-            JsonNode root = objectMapper.readTree(levelContent);
-            int mapHeight = root.get("height").asInt() * root.get("tileheight").asInt();
-
-            int playerCount = 0;
-            int starCount = 0;
-
-            for (JsonNode layer : root.get("layers")) {
-                if (!layer.get("type").asText().equals("objectgroup")) continue;
-
-                String layerName = layer.get("name").asText();
-
-                // Fill up the objects list
-                for (JsonNode obj : layer.get("objects")) {
-                    int x = obj.get("x").asInt();
-                    int y = mapHeight - obj.get("y").asInt() - obj.get("height").asInt();
-
-                    switch (layerName) {
-                        case "ground" -> objects.add(new FixedObject(new Transform(new Vector2(x, y), new Vector2(50, 50))));
-//                        case "player" -> {
-//                            objects.add(new Player(1, 300, new Transform(new Vector2(x, y), new Vector2(40, 80))));
-//                            playerCount++;
-//                        }
-                        case "coin" -> objects.add(ItemFactory.createCoin(x, y));
-                        case "banana" -> objects.add(ItemFactory.createMushroom(x, y));
-                        case "snail" -> objects.add(EnemyFactory.createSnail(x, y, EnemyType.SNAIL));
-                        case "leopard" -> objects.add(EnemyFactory.createLeopard(x, y, EnemyType.LEOPARD));
-//                        case "Star" -> starCount++; // TODO: implementer star tilfelle
-                        default -> System.out.println("Unknown layer: " + layerName + ". Case sensitivity maybe?");
-                    }
-                }
-            }
-//
-//            if (playerCount != 1) {
-//                throw new IllegalStateException("Level must have exactly one Player, but found: " + playerCount);
-//            }
-//            if (starCount != 1) {
-//                throw new IllegalStateException("Level must have exactly one Star, but found: " + starCount);
-//            }
-
+            levelContent = levelFile.readString();
+            jsonRoot = objectMapper.readTree(levelContent);
         } catch (IOException e) {
             throw new RuntimeException("Failed to load level: " + level);
         }
 
+        ArrayList<GameObject> objects = new ArrayList<>();
+        int mapHeight = jsonRoot.get("height").asInt() * jsonRoot.get("tileheight").asInt();
+        int playerCount = 0;
+        int starCount = 0;
+
+        for (JsonNode layer : jsonRoot.get("layers")) {
+            if (!layer.get("type").asText().equals("objectgroup")) continue;
+
+            // Fill up the objects list
+            String layerName = layer.get("name").asText();
+            for (JsonNode obj : layer.get("objects")) {
+                int x = obj.get("x").asInt();
+                int y = mapHeight - obj.get("y").asInt() - obj.get("height").asInt();
+
+                switch (layerName) {
+                    case "ground" -> objects.add(new FixedObject(new Transform(new Vector2(x, y), new Vector2(50, 50))));
+//                    case "player" -> {
+//                        objects.add(new Player(1, 300, new Transform(new Vector2(x, y), new Vector2(40, 80))));
+//                        playerCount++;
+//                    }
+                    case "coin" -> objects.add(ItemFactory.createCoin(x, y));
+                    case "banana" -> objects.add(ItemFactory.createMushroom(x, y));
+                    case "snail" -> objects.add(EnemyFactory.createSnail(x, y, EnemyType.SNAIL));
+                    case "leopard" -> objects.add(EnemyFactory.createLeopard(x, y, EnemyType.LEOPARD));
+                    case "Star" -> starCount++; // TODO: implementer star tilfelle
+                    default -> System.out.println("Unknown layer: " + layerName + ". Case sensitivity maybe?");
+                }
+            }
+        }
+        if (playerCount != 1) {
+            throw new IllegalStateException("Level must have exactly one Player, but found: " + playerCount);
+        }
+        if (starCount != 1) {
+            throw new IllegalStateException("Level must have exactly one Star, but found: " + starCount);
+        }
+
         return objects;
     }
+
+
+
+
+
+
 
     private static FileHandle getLevelFile(Level level) {
         String levelPath = "levels/";
