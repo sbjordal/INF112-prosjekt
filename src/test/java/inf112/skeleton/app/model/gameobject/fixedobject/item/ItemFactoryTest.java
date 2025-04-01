7d4c8d9571731bdb7ab77364475570aa11ecac20
Stine.Bjordal <stbjo9442@uib.no>
2025-04-01 15:09:36 +0200

    Skrevet banana-tester

--- src/test/java/inf112/skeleton/app/model/gameobject/fixedobject/item/ItemFactoryTest.java
+++ src/test/java/inf112/skeleton/app/model/gameobject/fixedobject/item/ItemFactoryTest.java
@@ -1,5 +1,7 @@
 package inf112.skeleton.app.model.gameobject.fixedobject.item;
 
+import com.badlogic.gdx.math.Vector2;
+import inf112.skeleton.model.gameobject.Transform;
 import inf112.skeleton.model.gameobject.fixedobject.item.Banana;
 import inf112.skeleton.model.gameobject.fixedobject.item.Coin;
 import inf112.skeleton.model.gameobject.fixedobject.item.ItemFactory;
@@ -81,11 +83,31 @@ public class ItemFactoryTest {
     }
 
     @Test
-    void testCreatedStarssAreNotSameObject() {
+    void testCreatedStarsAreNotSameObject() {
         Star star1 = ItemFactory.createStar(10, 10);
         Star star2 = ItemFactory.createStar(10, 10);
 
         assertNotEquals(star1, star2, "Two created stars should be different objects");
+    }
+
+    @Test
+    public void testGetBigJumpForce() {
+        Transform transform = new Transform(new Vector2(0, 0), new Vector2(1, 1));
+        Banana banana = new Banana(transform);
+
+        int expectedForce = 73000;
+        assertEquals(expectedForce, banana.getBigJumpForce(),
+                "getBigJumpForce() should return the correct jump force.");
+    }
+
+    @Test
+    public void testGetLargePlayerSize() {
+        Transform transform = new Transform(new Vector2(0, 0), new Vector2(1, 1));
+        Banana banana = new Banana(transform);
+
+        Vector2 expectedSize = new Vector2(65, 135);
+        assertEquals(expectedSize, banana.getLargePlayerSize(),
+                "getLargePlayerSize() should return the correct player size.");
     }
 
 }
