9f07eacb6a1e14d27593dd899f4acd57a62bef4f
@Endre.Aspoy <endre.aspoy@student.uib.no>
2025-04-07 17:21:27 +0200

    laget en ground klasse

---
+++ src/main/java/inf112/skeleton/model/gameobject/fixedobject/Ground.java
@@ -0,0 +1,14 @@
+package inf112.skeleton.model.gameobject.fixedobject;
+
+import inf112.skeleton.model.gameobject.Transform;
+
+public class Ground extends FixedObject {
+    /**
+     * Creates a new ground object with the specified transform.
+     *
+     * @param transform The fixed position and size of the Ground.
+     */
+    public Ground(Transform transform) {
+        super(transform);
+    }
+}
