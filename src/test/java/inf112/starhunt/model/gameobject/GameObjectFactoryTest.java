ce3b417fe160e9d1fc9348396811ade95056b5c6
Stine.Bjordal <stbjo9442@uib.no>
2025-05-02 11:55:08 +0200

    GameObject-tester

--- src/test/java/inf112/starhunt/model/gameobject/GameObjectFactoryTest.java
+++ src/test/java/inf112/starhunt/model/gameobject/GameObjectFactoryTest.java
@@ -1,6 +1,7 @@
 package inf112.starhunt.model.gameobject;
 
 import inf112.starhunt.model.gameobject.fixedobject.FixedObjectFactory;
+import inf112.starhunt.model.gameobject.fixedobject.Ground;
 import inf112.starhunt.model.gameobject.fixedobject.item.Banana;
 import inf112.starhunt.model.gameobject.fixedobject.item.Coin;
 import inf112.starhunt.model.gameobject.fixedobject.item.Star;
@@ -9,6 +10,8 @@ import inf112.starhunt.model.gameobject.mobileobject.actor.enemy.Enemy;
 import inf112.starhunt.model.gameobject.mobileobject.actor.enemy.Snail;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.function.BiFunction;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -41,34 +44,50 @@ public class GameObjectFactoryTest {
     }
 
     @Test
-    void shouldCreateBanana() {
+    void testShouldCreateBanana() {
         assertNotNull(banana);
         assertEquals(0f, banana.getTransform().getPos().x, 0.001f);
         assertEquals(0f, banana.getTransform().getPos().y, 0.001f);
     }
 
     @Test
-    void shouldCreatePlayer() {
+    void testShouldCreatePlayer() {
         assertNotNull(player);
         assertEquals(0f, player.getTransform().getPos().x, 0.001f);
         assertEquals(0f, player.getTransform().getPos().y, 0.001f);
     }
 
     @Test
-    void shouldCreateGroundWithAlteration() {
+    void testShouldCreateGroundWithAlteration() {
         assertNotNull(ground);
         assertEquals(0.0f, ground.getTransform().getPos().x, 0.001f);
         assertEquals(0.0f, ground.getTransform().getPos().y, 0.001f);
     }
 
     @Test
-    void shouldReturnCorrectObjectTypes() {
+    void testShouldReturnCorrectObjectTypes() {
+        assertTrue(ground instanceof Ground);
         assertTrue(banana instanceof Banana);
         assertTrue(star instanceof Star);
         assertTrue(coin instanceof Coin);
         assertTrue(enemy instanceof Snail);
         assertTrue(enemy instanceof Enemy);
         assertTrue(player instanceof Player);
+    }
+
+    @Test
+    void testShouldUseGroundBranch() {
+        GameObject ground = GameObjectFactory.createGameObject("ground_0000", 1f, 1f);
+        assertNotNull(ground);
+        assertEquals(1f, ground.getTransform().getPos().x, 0.001f);
+    }
+
+    @Test
+    void testRegistryShouldReturnValidObject() {
+        BiFunction<Float, Float, GameObject> creator = GameObjectFactory.registry.get("banana");
+        assertNotNull(creator);
+        GameObject obj = creator.apply(1.0f, 2.0f);
+        assertNotNull(obj);
     }
 
 
